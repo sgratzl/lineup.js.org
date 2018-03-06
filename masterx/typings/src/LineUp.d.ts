a2b89a4ee9d79f96f13e779444f085db191b6ae5
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-03-06 20:47:19 +0000

    auto updating masterx
    trigger: https://circleci.com/gh/sgratzl/lineupjsx/27

---
+++ masterx/typings/src/LineUp.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="react" />
+import { IBuilderAdapterProps, ITaggleOptions, LineUp as LineUpImpl, LocalDataProvider, Taggle } from 'lineupjs';
+import * as React from 'react';
+export declare type ILineUpProps = IBuilderAdapterProps;
+export default class LineUp extends React.PureComponent<Readonly<ILineUpProps>, {}> {
+    private node;
+    private readonly adapter;
+    protected createInstance(node: HTMLElement, data: LocalDataProvider, options: Partial<ITaggleOptions>): LineUpImpl | Taggle;
+    componentDidMount(): void;
+    componentDidUpdate(prevProps: Readonly<ILineUpProps>): void;
+    componentWillUnmount(): void;
+    render(): JSX.Element;
+}
