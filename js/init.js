507f24e093671383eab1ec547405ed5bb85e7d99
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-05-01 21:26:57 +0200

    optimize loading order

--- js/init.js
+++ js/init.js
@@ -1,3 +1,6 @@
+// lazy load google fonts
+document.head.insertAdjacentHTML('beforeend', '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">');
+
 $(function() {
   $(".dropdown-button").dropdown();
   $('.button-collapse').sideNav();
