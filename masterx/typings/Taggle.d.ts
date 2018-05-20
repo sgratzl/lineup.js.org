a7910a406923818c350c6d64e954978018cf2191
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-05-20 11:09:15 +0000

    auto updating masterx
    trigger: https://circleci.com/gh/datavisyn/lineupjsx/46

--- masterx/typings/Taggle.d.ts
+++
@@ -1,5 +0,0 @@
-import LineUp from './LineUp';
-import { LocalDataProvider, ITaggleOptions, Taggle as TaggleImpl } from 'lineupjs';
-export default class Taggle extends LineUp {
-    protected createInstance(node: HTMLElement, data: LocalDataProvider, options: Partial<ITaggleOptions>): TaggleImpl;
-}
--- masterx/typings/utils.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import * as React from 'react';
-export declare class ChildWrapper<T, P> {
-    readonly props: P;
-    readonly type: any;
-    constructor(props: P, type: any);
-    create(): T;
-}
-export declare function filterChildrenProps<T, P = any>(children: React.ReactNode, clazz: any): ChildWrapper<T, P>[];
