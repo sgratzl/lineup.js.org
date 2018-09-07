da3ff1ae524a25984dae8211b68a92bb03526d06
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-09-07 08:33:12 +0000

    auto updating app_develop
    trigger: https://circleci.com/gh/datavisyn/lineup_app/42

---
+++ app_develop/sw/precache-manifest.dce77e61bbca5cbd240a5d3119c68a6b.js
@@ -0,0 +1,176 @@
+self.__precacheManifest = [
+  {
+    "url": "e79bfd88537def476913f3ed52f4f4b3.eot"
+  },
+  {
+    "revision": "5df647286b01b2b73a05bde78e98d691",
+    "url": "preview.png"
+  },
+  {
+    "revision": "85e9582e84d2177c9b6c40340c328aef",
+    "url": "site.webmanifest"
+  },
+  {
+    "revision": "3af86681bfab89c499e24c9f126be1e2",
+    "url": "safari-pinned-tab.svg"
+  },
+  {
+    "url": "012cf6a10129e2275d79d6adac7f3b02.woff"
+  },
+  {
+    "url": "d45b122f4d403e27bd02.js"
+  },
+  {
+    "revision": "bb479153eca320aac13509ecb0285559",
+    "url": "mstile-310x310.png"
+  },
+  {
+    "revision": "b26043c045b5f9330c77aaeb545ac69c",
+    "url": "mstile-310x150.png"
+  },
+  {
+    "revision": "9d073a6904493c914226d682af3099b9",
+    "url": "mstile-150x150.png"
+  },
+  {
+    "revision": "b74e49a146b018e7e3dc39f56f3946a5",
+    "url": "mstile-144x144.png"
+  },
+  {
+    "url": "fee66e712a8a08eef5805a46892932ad.woff"
+  },
+  {
+    "revision": "705349e0c52c2a1eda332c083d566ec9",
+    "url": "mstile-70x70.png"
+  },
+  {
+    "revision": "ae2effb8025b1a997a784b6f7db915af",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "31767a1e0082f92641042338fb7ac8d5",
+    "url": "favicon-16x16.png"
+  },
+  {
+    "revision": "d3de2a6fcf606a24f223619e0dc7b788",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "10c6a8f6838fefbd38d793bb5a9f2bc4",
+    "url": "index.html"
+  },
+  {
+    "url": "fb2b3479194b0882abfd46d287f297ee.png"
+  },
+  {
+    "url": "f845bd16aa0f60d825543d688af4d4a8.png"
+  },
+  {
+    "url": "ec6cae2b78bd34737a76e09cfb311502.png"
+  },
+  {
+    "url": "ea36cd9a0e9eee97012a67b8a4570d7b.woff"
+  },
+  {
+    "url": "eed9aab5449cc9c8430d7d258108f602.woff"
+  },
+  {
+    "url": "ccda38f2ae7b5f07875dc4bba68b9586.svg"
+  },
+  {
+    "url": "c3640b066af0386fc542.js"
+  },
+  {
+    "url": "cf4d60bc0b1d4b2314085919a00e1724.woff"
+  },
+  {
+    "url": "c8d346bf1b045143cdf588e5eb848c86.png"
+  },
+  {
+    "revision": "d88bcfe86826896fe49ca5e009555a2f",
+    "url": "android-chrome-512x512.png"
+  },
+  {
+    "url": "5136cbe62a63604402f2fedb97f246f8.woff2"
+  },
+  {
+    "url": "570eb83859dc23dd0eec423a49e147fe.woff2"
+  },
+  {
+    "revision": "f24ccfb78b240c366e44",
+    "url": "app.css"
+  },
+  {
+    "url": "a37b0c01c0baf1888ca812cc0508f6e2.ttf"
+  },
+  {
+    "url": "c0f1e4a4fdfb8048c72e86aadb2a247d.woff2"
+  },
+  {
+    "url": "c0782860f7a105fa7eb9.js"
+  },
+  {
+    "url": "674f50d287a8c48dc19ba404d20fe713.eot"
+  },
+  {
+    "url": "af7ae505a9eed503f8b8e6982036873e.woff2"
+  },
+  {
+    "url": "5bfb840ce7e0f7204f6d4e849947f4fb.png"
+  },
+  {
+    "url": "b06871f281fee6b241d60582ae9369b9.ttf"
+  },
+  {
+    "url": "912ec66d7572ff821749319396470bde.svg"
+  },
+  {
+    "revision": "acb75e2c2da9562609eeb92c885de7a1",
+    "url": "android-chrome-192x192.png"
+  },
+  {
+    "revision": "f24ccfb78b240c366e44",
+    "url": "app.js"
+  },
+  {
+    "revision": "f46ece5ee98bcfb5fb6ffa8f15fa5f62",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "browserconfig.xml"
+  },
+  {
+    "url": "6dbea47babde65521717314b0f38d514.ttf"
+  },
+  {
+    "url": "4a1a2cd662b7e3116ca4.js"
+  },
+  {
+    "url": "44b78f142603eb69f593ed4002ed7a4a.woff"
+  },
+  {
+    "url": "3cf6adf61054c328b1b0ddcd8f9ce24d.woff"
+  },
+  {
+    "url": "3c37aa69cd77e6a53a067170fa8fe2e9.woff2"
+  },
+  {
+    "url": "3a206a43b77c7525e47a.js"
+  },
+  {
+    "url": "29334f20971b9251d88c2b7c1a082917.eot"
+  },
+  {
+    "url": "1f35e6a11d27d2e10d28946d42332dc5.woff2"
+  },
+  {
+    "url": "1a188825e914701bb88d47102f950811.png"
+  },
+  {
+    "url": "1561b424aaef2f704bbd89155b3ce514.woff2"
+  },
+  {
+    "url": "11b230833980dcd182b8.js"
+  }
+];
