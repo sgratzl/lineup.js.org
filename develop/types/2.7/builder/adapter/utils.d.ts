542bc4760ec6b9da0005b6a7ad027c22c7724cd1
Samuel Gratzl <sam@sgratzl.com>
2019-06-15 02:51:10 +0000

    auto updating develop
    trigger: https://circleci.com/gh/lineupjs/lineupjs/443

--- develop/types/2.7/builder/adapter/utils.d.ts
+++ develop/types/2.7/builder/adapter/utils.d.ts
@@ -1,4 +1,4 @@
 export { equal } from '../../internal';
 export declare function isTypeInstance(clazz: any, superClass: any): boolean;
-export declare function pick<T>(obj: T, keys: (keyof T)[]): Pick<T, keyof T>;
-export declare function isSame<T>(current: T, changed: (prop: keyof T) => boolean, props: (keyof T)[]): Pick<T, keyof T> | null;
+export declare function pick<T extends object>(obj: T, keys: (keyof T)[]): Pick<T, keyof T>;
+export declare function isSame<T extends object>(current: T, changed: (prop: keyof T) => boolean, props: (keyof T)[]): Pick<T, keyof T> | null;
