36a256e8a64033080803983eb0fa975357a9b5ce
thinkh <thinkh@users.noreply.github.com>
2021-03-24 07:43:52 +0000

    deploy: lineupjs/lineupjs@04323d9c5696e039b1a5fdfef311da7b8c599572

--- develop/src/builder/adapter/index.js
+++ develop/src/builder/adapter/index.js
@@ -1,2 +1,3 @@
-export * as builderAdapter from './builderAdapter';
+import * as builderAdapter_1 from './builderAdapter';
+export { builderAdapter_1 as builderAdapter };
 //# sourceMappingURL=index.js.map
